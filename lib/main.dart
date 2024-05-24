a32ffea01fd971d76981daaf54941482a81a07ae
Dealshot92 <dilshod.toshtemirov92@gmail.com>
2024-05-24 17:46:16 +0500

    added RootBinding, upgraded dependecies

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:bitcoin_news_app/config/root_binding.dart';
 import 'package:bitcoin_news_app/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +13,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home:  HomePage(),
+      initialBinding: RootBinding(),
     );
   }
 }
